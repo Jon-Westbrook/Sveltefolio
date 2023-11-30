d0a1a5b65ec3eb730e3350597cf73fc844ec58c8
Dave Seidman <daveseidman@Daves-MacBook-Pro.local>
2023-11-30 11:16:36 -0500

    testing luma as background

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,18 @@
-<h1>Welcome to the SvelteFolio!!!</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<iframe
+	class="scene"
+	src="https://lumalabs.ai/embed/711c40bc-3f17-45b5-81b4-b96c6bcd5df0?mode=sparkles&background=%23ffffff&color=%23000000&showTitle=true&loadBg=true&logoPosition=bottom-left&infoPosition=bottom-right&cinematicVideo=undefined&showMenu=false"
+	width="376"
+	height="500"
+	frameborder="0"
+	title="luma embed"
+	style="border: none;"
+></iframe>
+<div class="main">
+	<h1>Welcome to the SvelteFolio!!!</h1>
+	<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+</div>
+
+<style lang="scss">
+	@import './scene.scss';
+	@import './main.scss';
+</style>
